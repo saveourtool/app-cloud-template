496fd3a17a0bc05753075fbc50c95692cd710e50
Nariman Abdullin <nulls.narik@gmail.com>
2024-02-28 12:43:37 +0300

    added gradle task to start kafka and s3 storage

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.saveourtool.malware.detection.build.registerDockerService
+
 plugins {
     id("com.saveourtool.malware.detection.build.spring-boot-kotlin-configuration")
+    id("com.saveourtool.malware.detection.build.s3-local-run-configuration")
 }
 
 kotlin {
@@ -8,9 +11,11 @@ kotlin {
     sourceSets {
         jvmMain {
             dependencies {
-                implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//                implementation("org.springframework.boot:spring-boot-starter-data-jpa")
                 implementation("org.springframework.boot:spring-boot-starter-webflux")
             }
         }
     }
 }
+
+//project.registerS3Service("backend/src/jvmMain/resources/s3")
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.saveourtool.malware.detection.build.kafka-local-run-configuration")
+}
+
+group = "com.saveourtool.malware.detection"
