46e7b3894e4cd45bbb202fab434cfd3120c77a2b
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2024-03-13 02:20:49 +0300

    Fixing bootRun task
    ### What's done:
    - fixing the task using a workaround https://github.com/springdoc/springdoc-openapi-gradle-plugin/issues/121

--- gradle/plugins/src/main/kotlin/com/saveourtool/template/build/spring-boot-kotlin-configuration.gradle.kts
+++ gradle/plugins/src/main/kotlin/com/saveourtool/template/build/spring-boot-kotlin-configuration.gradle.kts
@@ -12,6 +12,11 @@ plugins {
     kotlin("plugin.serialization")
     id("org.springframework.boot")
     id("io.spring.dependency-management")
+    // • First project (Spring Boot 3.1.2, Gradle 8.1.1) was fixed by adding id("application") as first plugin. @nbam-e 's idea about moving Kotlin before Spring Boot also worked for me.
+    // • Second project (Spring Boot 2.7.14, Gradle 7.6.1) was fixed with same way as first project (by adding id("application")).
+    // • Third project (Spring Boot 2.7.14, Gradle 8.1.1) didn't work at all.
+    // Workaround from https://github.com/springdoc/springdoc-openapi-gradle-plugin/issues/121
+    application
 }
 
 @Suppress("GENERIC_VARIABLE_WRONG_DECLARATION")
